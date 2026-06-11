e3a4ff31f97edb568c6b30f632c35b2d514d35c4
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-11 18:08:05 +0600

    feat: add lesson and module management routes with role-based authorization

--- routes/lesson.routes.js
+++ routes/lesson.routes.js
@@ -10,5 +10,7 @@ router.post('/', protect, authorizeTeacher, controller.createLesson);
 router.get('/:id', protect, controller.getLessonById);
 router.put('/:id', protect, authorizeTeacher, controller.updateLesson);
 router.delete('/:id', protect, authorizeTeacher, controller.deleteLesson);
+router.put('/module/:id', protect, authorizeTeacher, controller.updateModule);
+router.delete('/module/:id', protect, authorizeTeacher, controller.deleteModule);
 
 module.exports = router;
