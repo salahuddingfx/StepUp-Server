4530ff255a587e50dffe3ba5c2ad07d42ec864ce
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-17 21:07:54 +0600

    feat: add express routes for assignment management and submission workflows

--- routes/assignment.routes.js
+++ routes/assignment.routes.js
@@ -6,6 +6,7 @@ const authorizeTeacher = require('../middlewares/teacher.middleware');
 const authorizeStudent = require('../middlewares/student.middleware');
 
 router.post('/', protect, authorizeTeacher, controller.createAssignment);
+router.delete('/:id', protect, authorizeTeacher, controller.deleteAssignment);
 router.post('/submit', protect, authorizeStudent, controller.submitAssignment);
 router.post('/grade', protect, authorizeTeacher, controller.gradeSubmission);
 router.get('/submission/:assignmentId', protect, authorizeTeacher, controller.getSubmissions);
