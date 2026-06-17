e6362ccb8584a6740d72782298fbd33a8187032b
salahuddingfx <salauddinkaderappy@gmail.com>
2026-06-17 23:07:27 +0600

    feat: implement certificate issuance service and routing with authentication middleware

--- routes/certificate.routes.js
+++ routes/certificate.routes.js
@@ -6,7 +6,9 @@ const authorizeTeacher = require('../middlewares/teacher.middleware');
 const authorizeAdmin = require('../middlewares/admin.middleware');
 
 router.get('/', protect, authorizeAdmin, controller.getAllCertificates);
+router.get('/my', protect, controller.getMyCertificates);
 router.get('/:id', protect, controller.getCertificateById);
 router.post('/issue', protect, authorizeTeacher, controller.generateCertificate);
+router.delete('/:id', protect, authorizeAdmin, controller.deleteCertificate);
 
 module.exports = router;
